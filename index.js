3fe9be6d00b038fa347934f84adcbfde910ab6c0
NoahNtabo23 <135032672+NoahNtabo23@users.noreply.github.com>
2023-06-19 08:48:53 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-var cards={
-    name:["Noah","Ben","Tom","John"]
-    eventName:["Birthday"]
-
+const cards=["Guadalupe", "Ollie", "Aki"]
+const event="Birhday"
+const messages=[]
+function writeCards(cards,event){
+    for(let i=0;i<cards.length;i++){
+        const message=`Thank you, ${cards[i]}, for the wonderful ${event} gift!`
+        messages.push(message)
+    }
+    return messages
 }
-function writeCards(cards){
-    for (let i = 0; i < cards.length; i++){
-        
+console.log(writeCards)
+
+function countDown(number){
+    while(number>=0){
+        console.log(number);
+        number--;
+
     }
 
 }
+countDown(10)
